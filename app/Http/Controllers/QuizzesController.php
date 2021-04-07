51f7ba1f16c075834a1e36a945ece9f54b6ac80b
Alejandro Naranjo <ale.naranjo.1792@gmail.com>
2021-04-07 16:07:05 -0400

    feat(logs)

--- app/Http/Controllers/QuizzesController.php
+++ app/Http/Controllers/QuizzesController.php
@@ -4,33 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Quiz;
+use Log;
 
 class QuizzesController extends Controller
 {
     public function index(Request $request)
     {
         $user = \Auth::user();
+        Log::info("User '$user->name' accessed to Quiz index");
         $quizzes = Quiz::where('user_id', $user->id)->get();
         if(count($quizzes) == 0) {
             return view('quiz.index');
         }
         foreach($quizzes as $quiz) {
             if (date('M-Y', strtotime($quiz->created_at)) == date('M-Y')) {
+                Log::info("User '$user->name' already has response quiz this month");
                 return view('quiz.detail', compact('quiz'));
             }
         }
+        Log::info("User '$user->name' needs to respond monthly quiz");
         return view('quiz.index');
     }
 
     public function store(Request $request)
     {
-        $user = \Auth::user();
-        $quiz = new Quiz();
-        $quiz->user_id = $user->id;
-        $quiz->note = $request->note;
-        $quiz->page_load_speed = $request->page_load_speed;
-        $quiz->page_reliability = $request->page_reliability;
-        $quiz->save();
-        return view('quiz.detail', compact('quiz'));
+        try {
+            $user = \Auth::user();
+            $quiz = new Quiz();
+            $quiz->user_id = $user->id;
+            $quiz->note = $request->note;
+            $quiz->page_load_speed = $request->page_load_speed;
+            $quiz->page_reliability = $request->page_reliability;
+            $quiz->save();
+            Log::info("User '$user->name' response was successfully stored.");
+            Log::info("Response Data: " . json_encode($quiz));
+            return view('quiz.detail', compact('quiz'));
+        } catch(\Exception $e) {
+            Log::error("User '$user->name' response produce an error while trying to store it.");
+            Log::error("Error: " . $e->getMessage());
+        }
+        return view('error.not_found');
     }
 }
--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Log;
 use App\Models\Quiz;
 
 class SummaryController extends Controller
@@ -12,8 +13,10 @@ class SummaryController extends Controller
     {
         $user = Auth::user();
         if (! $user->can('summary')) {
+            Log::warning("User '$user->name' try to access to summary.index page");
             return redirect('/quizzes');
         }
+        Log::info("Admin '$user->name' accessed to sumary page");
         $quizzes = Quiz::all();
         $page_reliability = $quizzes->countBy('page_reliability');
         return view('summary.index', compact('page_reliability'));
